31d0ecaa85cfac4ee36c45e441c8c9a0a7450226
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-03-19 12:37:47 +0530

    feat: Add various routes for core functionalities

--- BackendProject09/src/app.js
+++ BackendProject09/src/app.js
@@ -17,9 +17,22 @@ app.use(cookieParser())
 
 // routes import
 import userRouter from './routes/user.router.js'
+import tweetRouter from "./routes/tweet.router.js"
+import  helathcheckRouter  from './routes/healthcheck.router.js'
+import likeRouter from "./routes/like.router.js"
+import commentRouter from "./routes/comment.router.js"
+import videoRouter from "./routes/video.router.js"
+import dashboardRouter from "./routes/dashboard.router.js"
+
 
 // routes declaration
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/healthcheck", helathcheckRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
 
