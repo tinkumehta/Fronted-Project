2e272ffad91cf42916f12374ee58ab300086e850
Tinku kumar <158788018+tinkumehta@users.noreply.github.com>
2025-06-26 14:29:26 +0530

    feat: add authentication and post management components including Header, Login, Signup, PostCard, RTE, and PostForm

---
+++ Facebook/src/components/index.js
@@ -0,0 +1,28 @@
+import Button from "./Button"
+import Input from "./Input"
+import Select from "./Select"
+import Logo from "./logo"
+import Header from "./Header/Header"
+import LogoutBtn from "./Header/LogoutBtn"
+import AuthLayout from "./AuthLayout"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostCard from "./PostCard"
+import RTE from "./RTE"
+import Container from "./container/Container"
+
+export {
+    Button,
+    Input,
+    Select,
+    Logo,
+    Header,
+    Login,
+    LogoutBtn,
+    AuthLayout,
+    Signup,
+    PostCard,
+    RTE,
+    Container,
+    
+}
